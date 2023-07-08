1b6c4aa1109cb0421c3861c2f4e07aca9f36c9b9
emily <75251243+shyiyan@users.noreply.github.com>
2023-07-09 00:02:49 +1000

    Initial Commit

---
+++ MovieReminder/MovieReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieReminderApp.swift
+//  MovieReminder
+//
+//  Created by Emily Tian on 9/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
